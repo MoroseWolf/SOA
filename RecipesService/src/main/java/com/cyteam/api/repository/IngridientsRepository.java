50b59ebab02ebdae98e9bc17c563ddc32d2ffa63
MoroseWolf <maxfilimonenkov@yahoo.com>
2021-03-25 14:50:37 +0300

    Create IngridientsRepository.java

---
+++ RecipesService/src/main/java/com/cyteam/api/repository/IngridientsRepository.java
@@ -0,0 +1,7 @@
+package com.cyteam.api.repository;
+
+import com.cyteam.api.model.Ingridients;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IngridientsRepository extends JpaRepository<Ingridients, Long> {
+}
